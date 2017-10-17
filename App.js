d7e863c46ad16fef491a5a115cc1b9a2e7b71e82
Stephen Lightcap <stephen.lightcap01@utrgv.edu>
2017-10-16 23:55:42 -0500

    REST API

--- App.js
+++ App.js
@@ -8,6 +8,10 @@ import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View, Alert } from "react-native";
 import { Card, ListItem, Button } from "react-native-elements";
 import MapView from "react-native-maps";
+import PopupDialog, {
+  SlideAnimation,
+  DialogTitle
+} from "react-native-popup-dialog";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
@@ -17,44 +21,79 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component<{}> {
+  _loadTrucks() {
+      return fetch('http://www.bissliss.com/maps/get10Trucks2R.php?lat=25.946897833050684&lng=-97.49076255077038&pn=1&last=10')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        Alert.alert(JSON.stringify(responseJson));
+        return responseJson.truckID;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
-
-  _loadTrucks(){
-    Alert.alert('You tapped a button')
   }
 
   render() {
     return (
       <View style={styles.container}>
-      <View style={[styles.boxContainer, styles.boxOne]}>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
-      </View>
-      <View style={[styles.boxContainer, styles.boxThree]}>
-          <Card
-            title='HELLO WORLD'>
-            <Text style={{marginBottom: 10}}>
-              The idea with React Native Elements is more about component structure than actual design.
+        <View style={[styles.boxContainer, styles.boxOne]}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421
+            }}
+          />
+        </View>
+        <View style={[styles.boxContainer, styles.boxThree]}>
+          <Card title="HELLO WORLD">
+            <Text style={{ marginBottom: 10 }}>
+              The idea with React Native Elements is more about component
+              structure than actual design.
             </Text>
 
             <Button
-              onPress={this._loadTrucks}
-              icon={{name: 'code'}}
-              backgroundColor='#03A9F4'
-              fontFamily='Lato'
-              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
-              title='VIEW CURRENT TRUCKS' />
+              onPress={() => {
+                this.popupDialog.show();
+              }}
+              icon={{ name: "code" }}
+              backgroundColor="#03A9F4"
+              fontFamily="Lato"
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0
+              }}
+              title="VIEW CURRENT TRUCKS"
+            />
           </Card>
         </View>
+        <PopupDialog
+          ref={popupDialog => {
+            this.popupDialog = popupDialog;
+          }}
+          dialogAnimation={new SlideAnimation({ slideFrom: "bottom" })}
+          dialogTitle={<DialogTitle title="Find Nearest Trucks" />}
+        >
+          <Button
+            onPress={this._loadTrucks}
+            icon={{ name: "code" }}
+            backgroundColor="#03A9F4"
+            fontFamily="Lato"
+            buttonStyle={{
+              borderRadius: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginBottom: 0
+            }}
+            title="VIEW CURRENT TRUCKS"
+          />
+        </PopupDialog>
       </View>
-
     );
   }
 }
@@ -65,34 +104,34 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     bottom: 0,
-    position: 'absolute'
+    position: "absolute"
   },
   container: {
     flex: 1, // 1:1
-    flexDirection: 'column',
+    flexDirection: "column"
   },
   boxContainer: {
     flex: 1, // 1:3
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center"
   },
   boxOne: {
     flex: 3, // 3:6
-    backgroundColor: '#FFEEE4',
-    justifyContent: 'space-around',
-    alignItems: 'flex-start'
+    backgroundColor: "#FFEEE4",
+    justifyContent: "space-around",
+    alignItems: "flex-start"
   },
   boxTwo: {
     flex: 1, // 1:6
-    backgroundColor: '#F17F42'
+    backgroundColor: "#F17F42"
   },
   boxThree: {
     flex: 1, // 2:6
-    flexDirection: 'row',
-    justifyContent: 'space-between', // main axis
-    alignItems: 'flex-end', // cross axis
-    backgroundColor: '#CE6D39',
-  },
+    flexDirection: "row",
+    justifyContent: "space-between", // main axis
+    alignItems: "flex-end", // cross axis
+    backgroundColor: "#CE6D39"
+  }
   // mainContainer: {
   //   flex: 1,
   //   alignItems: 'center',
